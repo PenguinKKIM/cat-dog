d7bc8cd5f3ac41fbf37a4fa6534b4859e76cf206
PenguinKKIM <103123935+PenguinKKIM@users.noreply.github.com>
2024-11-19 20:10:16 +0900

    refactor : 라우터 수정

--- src/router.tsx
+++ src/router.tsx
@@ -1,44 +1,45 @@
-import { createBrowserRouter } from "react-router-dom";
-import Layout from "./pages/layout";
-import Main from "./components/Main/Main";
-import Around from "./components/Around/Around";
-import Gallery from "./components/Gallery/Gallery";
-import UserGallery from "./components/Gallery/UserGallery/UserGallery";
-import DesignerGallery from "./components/Gallery/DesignerGallery/DesignerGallery";
-import Login from "./components/Login/Login";
-import PetRegistration from "./components/PetRegistration/PetRegistration";
+import { createBrowserRouter } from 'react-router-dom';
+import Layout from './pages/layout';
+import Main from './components/Main/Main';
+import Around from './components/Around/Around';
+import Gallery from './components/Gallery/Gallery';
+import UserGallery from './components/Gallery/UserGallery/UserGallery';
+import DesignerGallery from './components/Gallery/DesignerGallery/DesignerGallery';
+import Login from './components/Login/Login';
+// import PetRegistration from './components/PetRegistration/PetRegistration';
+import PetRegistrationList from './components/PetRegistration/PetRegistrationList';
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <Layout />,
     children: [
       {
-        path: "",
+        path: '',
         element: <Main />,
       },
       {
-        path: "/around",
+        path: '/around',
         element: <Around />,
       },
       {
-        path: "/login",
+        path: '/login',
         element: <Login />,
       },
       {
-        path: "/petreg",
-        element: <PetRegistration />,
+        path: '/petreg',
+        element: <PetRegistrationList />,
       },
       {
-        path: "/gallery",
+        path: '/gallery',
         element: <Gallery key={location.pathname} />,
         children: [
           {
-            path: "usergal",
+            path: 'usergal',
             element: <UserGallery />,
           },
           {
-            path: "designergal",
+            path: 'designergal',
             element: <DesignerGallery />,
           },
         ],
